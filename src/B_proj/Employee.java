4d0e384ef7883d3b0dda63eaa0ac89f9f698a09b
BrianoBruno <briano.bruno@cix.csi.cuny.edu>
2017-04-13 17:00:41 -0400

    Model's Finished

--- src/B_proj/Employee.java
+++ src/B_proj/Employee.java
@@ -1,5 +1,35 @@
 package B_proj;
 
 public class Employee extends Person{
-
+	int hourlyRate;
+	int sales;
+	String username;
+	String password;
+	
+	public int getHourlyRate() {
+		return hourlyRate;
+	}
+	public void setHourlyRate(int hourlyRate) {
+		this.hourlyRate = hourlyRate;
+	}
+	public int getSales() {
+		return sales;
+	}
+	public void setSales(int sales) {
+		this.sales = sales;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 }
