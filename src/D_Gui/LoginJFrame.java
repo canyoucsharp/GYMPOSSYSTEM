e105987e903a7f63fdff5d44701f384a5a4ced9d
dennikra <denniskravtsov95@gmail.com>
2017-04-13 17:50:48 -0400

    ui enter press

--- src/D_Gui/LoginJFrame.java
+++ src/D_Gui/LoginJFrame.java
@@ -78,11 +78,20 @@ ResultSet rs=null;
 		lblPassword.setBounds(10, 82, 74, 15);
 		contentPane.add(lblPassword);
 		
+		but1.addKeyListener(new KeyListener() {
+		    // listen to keys
+		    public void keyPressed(KeyEvent e){
+		        // find ENTER key press
+		        if(e.getKeyCode() == KeyEvent.VK_ENTER){
+		            goto button;
+		        }
+		    }
+		}
+		
 		Button button = new Button("Login");
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
-				//System.out.println("Hello");
+				button:
 				try
 				{
 					String query="select * from salesrep where username=? and password=?";
