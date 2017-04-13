c9dce08a264516fb6a92bb60b945289b80057e04
canyoucsharp <mkholodovsky@gmail.com>
2017-04-13 17:00:32 -0400

    connection

--- src/B_proj/CustomerDb.java
+++ src/B_proj/CustomerDb.java
@@ -1,10 +1,14 @@
 package B_proj;
 
+import M_Database.MysqlConnect;
+
 public class CustomerDb {
 
 	public static void init(Customer cusObj) {
 		// TODO Auto-generated method stub
-		
+	MysqlConnect.ConnectDB();
+	
+	
 	}
 
 }
