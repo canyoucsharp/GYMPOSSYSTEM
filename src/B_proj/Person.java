d3f1db64df6beb9898233a587b89cbcf75fa2a25
BrianoBruno <briano.bruno@cix.csi.cuny.edu>
2017-04-13 15:21:20 -0400

    Person Base Class Implemented

--- src/B_proj/Person.java
+++ src/B_proj/Person.java
@@ -1,5 +1,48 @@
 package B_proj;
 
-public class person {
+public class Person {
 
+	String firstName;
+	String lastName;
+	int phoneNumber;
+	String address;
+	int age;
+	char sex;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public char getSex() {
+		return sex;
+	}
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
 }
