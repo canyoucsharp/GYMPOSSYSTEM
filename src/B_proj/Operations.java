edf2745064b7ca1531ad3b700e410a40af718fe5
BrianoBruno <briano.bruno@cix.csi.cuny.edu>
2017-04-30 15:10:28 -0400

    op

--- src/B_proj/Operations.java
+++ src/B_proj/Operations.java
@@ -1,7 +1,38 @@
 package B_proj;
 
 public class Operations {
-	int overhead;
-	int locationid;
+	private int locationCost;
+	private Utility utilityObj;
+	private int accountsPayable;
+	private int maxOccupancy;
+	private double monthlyCost;
+	private double roi;
+	Operations(){
+		utilityObj=new Utility();
+	}
+	public int getLocationCost() {
+		return locationCost;
+	}
+	public void setLocationCost(int locationCost) {
+		this.locationCost = locationCost;
+	}
+	public int getAccountsPayable() {
+		return accountsPayable;
+	}
+	public void setAccountsPayable(int accountsPayable) {
+		this.accountsPayable = accountsPayable;
+	}
+	public int getMaxOccupancy() {
+		return maxOccupancy;
+	}
+	public void setMaxOccupancy(int maxOccupancy) {
+		this.maxOccupancy = maxOccupancy;
+	}
+	public void calculateMonthlyCost(){
+		monthlyCost=(locationCost+accountsPayable+utilityObj.totalUtilities());
+		roi+=1;
+		monthlyCost*=roi;
+		monthlyCost/=maxOccupancy;
+	}
 	
 }
